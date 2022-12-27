723ce071d88f15422ea9a6052af92637ab1aae19
Janelle Kiker <janellekiker@gmail.com>
2022-12-27 14:48:33 -0600

    added employees object

--- youtubereact/src/App.js
+++ youtubereact/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Header from "./Header";
-import Content from "./Content";
 import Footer from "./Footer";
+import Employees from "./Employees";
 
 function App() {
   return (
     <div>
       <Header />
-      <Content />
+      <Employees />
       <Footer />
     </div>
   );
