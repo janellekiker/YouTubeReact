65a40e890709c16ecfbc0eec62a4b9e11e5cb2c5
Janelle Kiker <janellekiker@gmail.com>
2022-12-28 16:53:31 -0600

    styled employee info

--- youtubereact/src/Employees.js
+++ youtubereact/src/Employees.js
@@ -3,6 +3,8 @@ import femaleProfile from "./images/femaleProfile.jpg";
 import maleProfile from "./images/maleProfile.jpg";
 
 function Employees() {
+  const [selectedTeam, setTeam] = useState("TeamB");
+
   const [employees, setEmployees] = useState([
     {
       id: 1,
@@ -90,7 +92,72 @@ function Employees() {
     },
   ]);
 
-  return <main></main>;
+  function handleTeamSelectionChange(event) {
+    setTeam(event.target.value);
+  }
+
+  function handleEmployeeCardClick(event) {
+    const transformedEmployees = employees.map((employee) =>
+      employee.id === parseInt(event.currentTarget.id)
+        ? employee.TeamName === selectedTeam
+          ? { ...employee, teamName: "" }
+          : { ...employee, teamName: selectedTeam }
+        : employee
+    );
+    setEmployees(transformedEmployees);
+  }
+
+  return (
+    <main className="container">
+      {/*Team Selector Dropdown*/}
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <select
+            className="form-select form-select-lg"
+            value={selectedTeam}
+            onChange={handleTeamSelectionChange}
+          >
+            <option value="TeamA">TeamA</option>
+            <option value="TeamB">TeamB</option>
+            <option value="TeamC">TeamC</option>
+            <option value="TeamD">TeamD</option>
+          </select>
+        </div>
+      </div>
+      {/*Employee Cards*/}
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <div className="card-collection">
+            {employees.map((employee) => (
+              <div
+                id={employee.id}
+                className={
+                  employee.teamName === selectedTeam
+                    ? "card m-2 standout"
+                    : "card m-2"
+                }
+                style={{ cursor: "pointer" }}
+                onClick={handleEmployeeCardClick}
+              >
+                {employee.gender === "male" ? (
+                  <img src={maleProfile} className="card-img-top" />
+                ) : (
+                  <img src={femaleProfile} className="card-img-top" />
+                )}
+
+                <div className="card-body">
+                  <h5 className="card-title">Full Name: {employee.fullName}</h5>
+                  <p className="card-text">
+                    <b>Designation:</b> {employee.designation}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Employees;
