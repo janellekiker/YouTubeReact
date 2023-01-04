d34def59d7cdbb56a614c3620a3dd0c3ec1d3bbe
Janelle Kiker <janellekiker@gmail.com>
2023-01-04 09:06:19 -0600

    Finished website

--- youtubereact/src/GroupedTeamMembers.js
+++ youtubereact/src/GroupedTeamMembers.js
@@ -12,8 +12,9 @@ function GroupedTeamMembers({ employees, selectedTeam, setTeam }) {
     let teamA = {
       team: "TeamA",
       members: teamAMembers,
-      collapsed: selectedTeam === "TeamA" ? flase : true,
-    }teams.push(teamA);
+      collapsed: selectedTeam === "TeamA" ? false : true,
+    };
+    teams.push(teamA);
 
     let teamBMembers = employees.filter(
       (employee) => employee.teamName === "TeamB"
@@ -21,8 +22,9 @@ function GroupedTeamMembers({ employees, selectedTeam, setTeam }) {
     let teamB = {
       team: "TeamB",
       members: teamBMembers,
-      collapsed: selectedTeam === "TeamB" ? flase : true,
-    }teams.push(teamB);
+      collapsed: selectedTeam === "TeamB" ? false : true,
+    };
+    teams.push(teamB);
 
     let teamCMembers = employees.filter(
       (employee) => employee.teamName === "TeamC"
@@ -30,8 +32,9 @@ function GroupedTeamMembers({ employees, selectedTeam, setTeam }) {
     let teamC = {
       team: "TeamC",
       members: teamCMembers,
-      collapsed: selectedTeam === "TeamC" ? flase : true,
-    }teams.push(teamC);
+      collapsed: selectedTeam === "TeamC" ? false : true,
+    };
+    teams.push(teamC);
 
     let teamDMembers = employees.filter(
       (employee) => employee.teamName === "TeamD"
@@ -39,46 +42,60 @@ function GroupedTeamMembers({ employees, selectedTeam, setTeam }) {
     let teamD = {
       team: "TeamD",
       members: teamDMembers,
-      collapsed: selectedTeam === "TeamD" ? flase : true,
-    }teams.push(teamD);
+      collapsed: selectedTeam === "TeamD" ? false : true,
+    };
+    teams.push(teamD);
 
     return teams;
   }
 
-function handleTeamClick(event) {
-  let transformedGroupData = groupedEmployees.map((groupedData) => groupedData.team === event.currentTarget.id ? {...groupedData,collapsed:!groupedData.collapsed}:groupedData)
+  function handleTeamClick(event) {
+    let transformedGroupData = groupedEmployees.map((groupedData) =>
+      groupedData.team === event.currentTarget.id
+        ? { ...groupedData, collapsed: !groupedData.collapsed }
+        : groupedData
+    );
 
-setGroupedData(transformedGroupData);
-setTeam(event.currentTarget.id);
-}
+    setGroupedData(transformedGroupData);
+    setTeam(event.currentTarget.id);
+  }
 
   return (
     <main className="container">
       {groupedEmployees.map((item) => {
         return (
-          <div key={item.team}
-          className='card mt-2'
-          style={{cursor:'pointer'}}>
-            <h4 id={item.team}
-            className='card-header text-secondary bg-white'
-            onclick={handleTeamClick}>Team Name: {item.team}</h4>
-            <div id={'colapse_' + item.team}
-            className={item.collapsed === true?'collapsed' : ''}>
-              < hr/>
-              {item.members.map(member => {
+          <div
+            key={item.team}
+            className="card mt-2"
+            style={{ cursor: "pointer" }}
+          >
+            <h4
+              id={item.team}
+              className="card-header text-secondary bg-white"
+              onclick={handleTeamClick}
+            >
+              Team Name: {item.team}
+            </h4>
+            <div
+              id={"collapse_" + item.team}
+              className={item.collapsed === true ? "collapse" : ""}
+            >
+              <hr />
+              {item.members.map((member) => {
                 return (
                   <div className="mt-2">
                     <h5 className="card-title mt-2">
-                      <span className="text-dark">Full Name: {member.fullName}</span>
+                      <span className="text-dark">
+                        Full Name: {member.fullName}
+                      </span>
                     </h5>
                     <p>Designation: {member.designation}</p>
                   </div>
                 );
               })}
-
             </div>
           </div>
-        )
+        );
       })}
     </main>
   );
