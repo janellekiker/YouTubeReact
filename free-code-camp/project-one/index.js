f0aac2b591863f9af98bb7793f23e39d08995dbb
Janelle Kiker <janellekiker@gmail.com>
2023-03-08 13:40:33 -0600

    new project

---
+++ free-code-camp/project-one/index.js
@@ -0,0 +1,14 @@
+function MainContent() {
+  return (
+    <nav>
+      <h1></h1>
+    </nav>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <MainContent />
+  </div>,
+  document.getElementById("root")
+);
