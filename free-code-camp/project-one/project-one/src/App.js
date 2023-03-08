a7cf238039b7bf418d4af99ab218ca1a2c93322e
Janelle Kiker <janellekiker@gmail.com>
2023-03-08 17:47:04 -0600

    Finished website

--- free-code-camp/project-one/project-one/src/App.js
+++ free-code-camp/project-one/project-one/src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
         {/* These are out different paths to different webpages */}
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/menu" exact element={<Menu />} />
+          <Route path="/about" exact element={<About />} />
+          <Route path="/contact" exact element={<Contact />} />
         </Routes>
         {/* We want to keep this on every page */}
         <Footer />
